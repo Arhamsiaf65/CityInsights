1b55151591d72c393ce342432a02ab1098906d94
ArhamSaif <arhamsaif65@gmail.com>
2025-05-07 00:08:14 +0500

    added patch req in allowed headers

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
     origin: [
         'https://city-insight-sigma.vercel.app',
-        'https://city-insights-nine.vercel.app'
+        'https://city-insights-nine.vercel.app',
+        'http://localhost:5173'
     ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
 
 
